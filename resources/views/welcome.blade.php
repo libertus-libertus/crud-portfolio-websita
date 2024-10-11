95a0af75279a5f6237ed7967a7362bf91679daee
libertus-libertus <libert.jobs@gmail.com>
2024-10-11 23:38:39 +0700

    instalasi dan routing

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Landing Page Website</title>
+    <style>
+        div {
+            height: 90vh;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            align-content: center;
+        }
+        div .kedua {
+            display: flex;
+            flex-direction: column;
+            margin: 0px !important;
+            padding: 0px !important;
+        }
+    </style>
+</head>
+<body>
+    <div>
+        <div class="kedua">
+            <h1>Landing Page Website</h1>
+            <h2>Laravel REST Full API</h2>
+        </div>
+    </div>
+</body>
+</html>
