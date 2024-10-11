8d4321a11e7c878dfb0d4acd68e8118c8dccd3f0
libertus-libertus <libert.jobs@gmail.com>
2024-10-12 00:22:52 +0700

    membuat controller sekaligus menampilkan data

---
+++ app/Http/Controllers/WargaController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Warga;
+use Illuminate\Http\Request;
+
+class WargaController extends Controller
+{
+    public function index() {
+        $warga = Warga::all();
+        return view('warga.index', compact('warga'));
+    }
+}
---
+++ resources/views/warga/index.blade.php
@@ -0,0 +1,22 @@
+<table border="1" cellspacing="0" cellpadding="10">
+    <tr>
+        <th>Nomor</th>
+        <th>Nama</th>
+        <th>Nomor NIK</th>
+        <th>Nomor KK</th>
+        <th>Gender</th>
+        <th>Alamat</th>
+    </tr>
+    @forelse ($warga as $item)
+    <tr>
+        <td>{{ $loop->iteration }}</td>
+        <td>{{ $item->nama }}</td>
+        <td>{{ $item->nik }}</td>
+        <td>{{ $item->no_kk }}</td>
+        <td>{{ $item->jenis_kelamin }}</td>
+        <td>{{ $item->alamat }}</td>
+    </tr>
+    @empty
+    <span>Belum Ada Data</span>
+    @endforelse
+</table>
