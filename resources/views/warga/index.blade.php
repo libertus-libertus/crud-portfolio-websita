c4f6c281e98e66f48fd20aae22cf772d38b65fb4
libertus-libertus <libert.jobs@gmail.com>
2024-10-12 11:03:36 +0700

    authentikasi dan selesai

--- resources/views/warga/index.blade.php
+++ resources/views/warga/index.blade.php
@@ -1,42 +1,53 @@
-@extends('layouts.master')
+@extends('layouts.app')
 
 @section('pageTitle', 'Dashboard')
 @section('content')
-<div class="container my-2">
-    <a href="{{ route('warga.create.page') }}" class="btn btn-primary btn-sm mb-2">Tambah Data Baru</a>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">{{ __('Data Penduduk') }}</div>
 
-    <table class="table table-bordered table-striped">
-        <thead>
-            <th>Nomor</th>
-            <th>Nama</th>
-            <th>Nomor NIK</th>
-            <th>Nomor KK</th>
-            <th class="text-center">Gender</th>
-            <th>Alamat</th>
-            <th>Aksi</th>
-        </thead>
-        <tbody>
-            @forelse ($warga as $item)
-            <td>{{ $loop->iteration }}</td>
-            <td>{{ $item->nama }}</td>
-            <td>{{ $item->nik }}</td>
-            <td>{{ $item->no_kk }}</td>
-            <td class="text-center">{{ $item->jenis_kelamin }}</td>
-            <td>{{ $item->alamat }}</td>
-            <td>
-                <div class="btn-group">
-                    <a href="{{ route('warga.edit.page', $item->id) }}" class="btn btn-sm btn-warning">Edit</a>
-                    <form action="{{ route('warga.destroy.page', $item->id) }}" method="post">
-                        @csrf
-                        @method('delete')
-                        <input type="submit" value="Hapus" class="btn btn-sm btn-danger">
-                    </form>
+                <div class="card-body">
+                    <a href="{{ route('warga.create.page') }}" class="btn btn-primary btn-sm mb-2">Tambah Data Baru</a>
+
+                    <table class="table table-striped">
+                        <thead>
+                            <th>NO.</th>
+                            <th>NAMA</th>
+                            <th>NOMOR NIK</th>
+                            <th>NOMOR KK</th>
+                            <th class="text-center">JENIS KELAMIN</th>
+                            <th>ALAMAT</th>
+                            <th>AKSI</th>
+                        </thead>
+                        <tbody>
+                            @forelse ($warga as $item)
+                            <td>{{ $loop->iteration }}</td>
+                            <td>{{ $item->nama }}</td>
+                            <td>{{ $item->nik }}</td>
+                            <td>{{ $item->no_kk }}</td>
+                            <td class="text-center">{{ $item->jenis_kelamin }}</td>
+                            <td>{{ $item->alamat }}</td>
+                            <td>
+                                <div class="btn-group">
+                                    <a href="{{ route('warga.edit.page', $item->id) }}"
+                                        class="btn btn-sm btn-warning">Edit</a>
+                                    <form action="{{ route('warga.destroy.page', $item->id) }}" method="post">
+                                        @csrf
+                                        @method('delete')
+                                        <input type="submit" value="Hapus" class="btn btn-sm btn-danger">
+                                    </form>
+                                </div>
+                            </td>
+                        </tbody>
+                        @empty
+                        <span class="text-primary">Belum Ada Data</span>
+                        @endforelse
+                    </table>
                 </div>
-            </td>
-        </tbody>
-        @empty
-        <span class="text-primary">Belum Ada Data</span>
-        @endforelse
-    </table>
+            </div>
+        </div>
+    </div>
 </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\WargaController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,9 +19,15 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/warga', [WargaController::class, 'index'])->name('warga.page');
-Route::get('/warga/create', [WargaController::class, 'create'])->name('warga.create.page');
-Route::post('/warga/store', [WargaController::class, 'store'])->name('warga.store.page');
-Route::get('/warga/{id}/edit', [WargaController::class, 'edit'])->name('warga.edit.page');
-Route::put('/warga/{id}', [WargaController::class, 'update'])->name('warga.update.page');
-Route::delete('/warga/{id}', [WargaController::class, 'destroy'])->name('warga.destroy.page');
+Route::middleware(["auth"])->group(function () {
+    Route::get('/warga', [WargaController::class, 'index'])->name('warga.page');
+    Route::get('/warga/create', [WargaController::class, 'create'])->name('warga.create.page');
+    Route::post('/warga/store', [WargaController::class, 'store'])->name('warga.store.page');
+    Route::get('/warga/{id}/edit', [WargaController::class, 'edit'])->name('warga.edit.page');
+    Route::put('/warga/{id}', [WargaController::class, 'update'])->name('warga.update.page');
+    Route::delete('/warga/{id}', [WargaController::class, 'destroy'])->name('warga.destroy.page');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
